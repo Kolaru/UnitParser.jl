7ec354f19b134f0bdfe43269a679b61a46f116e9
Benoît Richard <benoit.richard@desy.de>
2023-02-13 17:22:56 +0100

    Some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using UnitParser
 using Test
 
 @testset "UnitParser.jl" begin
-    # Write your tests here.
+    @test reduce_units_expr("meter/seconds") == "m*s^-1"
+    @test reduce_units_expr("meter seconds^-1") == "m*s^-1"
+    @test reduce_units_expr("nanometers3 / microsecond") == "nm^3*μs^-1"
+    @test reduce_units_expr("volt^2/kiloamp") == "V^2*kA^-1"
+    @test reduce_units_expr("milligram/metres2") == "mg*m^-2"
 end
